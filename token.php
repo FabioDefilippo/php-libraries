5ff373a16c97270e3c65473f1ca7d5a3e868ad19
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2022-05-23 22:03:03 +0200

    Update token.php

--- token.php
+++ token.php
@@ -1,5 +1,5 @@
 <?php
-function generate_hex(string $algo, $number, string $salt)
+function generate_hex(string $algo = 'sha256', $number = 63, string $salt)
 {
  return hash_hmac($algo, bin2hex(random_bytes($number)), $salt, false);
 }
