474dadcf51f0251a8ef3836dfd4cfe9f633a1f77
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2022-05-24 16:10:30 +0200

    Update token.php

--- token.php
+++ token.php
@@ -4,7 +4,7 @@ function generate_hex(string $salt, string $algo = 'sha256', $number = 63)
  return hash_hmac($algo, bin2hex(random_bytes($number)), $salt, false);
 }
 
-function compare_tokens(string $tokenClient, string $tokenServer, string & $err_message = "")
+function compare_tokens(string $tokenClient, string $tokenServer, string &$err_message = "")
 {
  if(hash_equals($tokenClient, $tokenServer))
  {
